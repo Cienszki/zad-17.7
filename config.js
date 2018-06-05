ce0faf33620f71672a4bafdc18050ea6492de211
Cienszki <wilq.wdz@gmail.com>
2018-06-05 18:25:45 +0200

    Add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '462031734797-cs9fj798u8ppn48nig5v8tuu74falvdh.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'qiOnAde6C3zYsdr6nHE-2Ftb',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
